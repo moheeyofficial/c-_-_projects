293b456f36a1656725294a0af3160408492e42ce
moheeyofficial <info@mahmoudmohey.com>
2023-05-18 11:18:50 +0300

    I created a code whose idea is a simple puzzle, in C++

---
+++ guess_the_number.cpp
@@ -0,0 +1,27 @@
+/*I'm glad you saw my code
+I made this code, I am Mahmoud Mohey. I've created code whose idea is a simple puzzle, in C++
+On May 18, 2023 while I was studying studying Fundamentals Of Programming With C++.
+You can find out more about me and contact me through my website.
+www.mahmoudmohey.com */
+
+#include <iostream> 
+using namespace std;
+int main ()
+{
+    cout << "What number, if divided by any number, equals the same product?   " ;
+
+    int input ;
+    int righ_number = 0 ;
+    
+    cin >> input ; 
+
+        if(input==righ_number)
+            {
+                cout << "good The solution is correct" ;
+            }
+        if(input!=righ_number)
+            {
+                cout << " Try again  The solution is wrong" ;
+            }
+return 0;
+}
