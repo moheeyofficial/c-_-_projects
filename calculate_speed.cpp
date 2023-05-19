227f7827f55a651ee1bbc4890a52cc28a3fa27d8
moheeyofficial <info@mahmoudmohey.com>
2023-05-19 11:14:30 +0300

    قمت بتغيرنوع بعض المتغيرات و قمت بإضافة شرط لقيمة الزمن

--- calculate_speed.cpp
+++ calculate_speed.cpp
@@ -12,29 +12,32 @@ using namespace std;
 int main()
 {
     cout << "===========================\n";
-    cout << "==  calculate speed   ==\n";
+    cout << "====  calculate speed   ===\n";
     cout << "===========================\n";
-
-    int distance;
-    int time;
+    // تعديل 19 مايو 2023 
+    // قمت بتغير نوع البيانات  بحيث تقبل الكسور 
+    float distance;
+    float time;
 
     cout << "wite the distance in metres  ";
     cin >> distance;
-    cout << "distance in metres is  ";
-    cout << distance;
-    cout << " m\n";
+    cout << "distance in metres is  " << distance  << " m\n";
 
     cout << " \nwite the time in seconds  ";
     cin >> time;
-    cout << "time in seconds is  ";
-    cout << time;
-    cout << " s\n";
+    // تعديل 19 مايو 2023 
+    //أضفت الدالة بحيث لا يمكن إدخال قيمة للزمن اقل من أو تساوي صفر 
+        if (time <= 0)
+            {
+                cout << "Go back, write the time correctly time in seconds is not   "<< time ;
+            }
+        else
+            {
+                cout<<"time in seconds is " << time << "\n";
 
-    int speed = distance / time;
-    cout << "\nspeed is  ";
-    cout << speed;
-    cout << " m/s";
-    cout << "\nNote: Speed is displayed to the nearest whole number";
+                float speed = distance / time;
+                cout << "\nspeed is  " << speed << " m/s";
+            }
 
     return 0;
 }
